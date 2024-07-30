01fb61809d6208a6c752296a36ec0e8e1ae40467
Ayesha-o5 <ash.asim.o5@gmail.com>
2024-07-30 17:34:19 +0500

    javascript code

---
+++ app.js
@@ -0,0 +1,480 @@
+// chapter 1
+alert("First Name");
+
+alert("Last Name");
+
+alert("Email");
+
+alert("Phone number");
+
+alert("Password");
+
+alert("You're learning JavaScript!");
+
+alert("I am learning JavaScript from Jawan Pakistan");
+
+// chapter 2
+// 1
+var myName=("Ayesha");
+
+// 2
+var myVariable;
+var myVariable=("Hello World!");
+
+// 3
+var teamName=("Karachi Kings");
+alert(teamName);
+
+// 4
+var bestMan=("Charlie");
+bestMan=("John");
+
+// chapter 3
+
+// 1
+var caseQty;
+
+// 2
+var caseQty=144;
+
+// 3
+var num=9;
+
+// 4
+var sum;
+sum=10+5;
+
+// 5
+var merchTotal = 100;
+var shippingCharge = 10;
+var orderTotal = merchTotal + shippingCharge;
+var orderTotal = 100+10;
+var orderTotal = 110;
+
+// 6
+var number= 20;
+var y=(number+10);
+
+// chapter 4 
+
+// 1 
+var productCost = 3.45;
+
+// 2
+var NameOfBand;
+
+// 3
+
+var $num=20;
+
+// 4
+var myNames="Ayesha Asim";
+
+// 5
+// Legal variables:
+
+
+firstName
+lastName
+user_age
+productCost
+a123
+$price
+_temp
+
+// Illegal variables:
+
+// 1name
+// ?name
+// .abc
+// abc name
+
+// chapter 5 
+//  1
+// name= Modulus
+// symbol = %
+
+// 2
+
+var num3= 20%6;
+num3=2;
+
+// 3 
+ var largeNum=2000000;
+
+//  4
+var a= 20;
+var b= 10;
+var c= a-b;
+
+// 5
+var num=20%6
+
+
+// 6
+var a3= 5;
+var b4= 2;
+var c5= a*b;
+alert(c5);
+
+
+// chapter 6 
+
+// 1
+++x
+
+// 2
+
+--x
+
+// 3
+var x = 50;
+var y = x++;
+var y = 50;
+
+// 4 
+var y = 50;
+var z = --y;
+var z = 49;
+
+// 5
+
+var newNum = a--;
+
+// 6 
+
+var a = a++;
+
+// 7
+
+var x = 10;
+var y = x++;
+alert(y);
+
+// chapter 7 
+
+// 1
+
+var calculatedNum = 2 + (2 * 6);
+    calculatedNum = 14;
+
+    // 2
+
+    var calculatedNum = (2 + 2) * 6;
+        calculatedNum = 24;
+
+//   3 
+
+var calculatedNum = (2 + 2) * (4 + 2);
+calculatedNum = 24;
+
+// 4 
+
+var calculatedNum = ((2 + 2) * 4) + 2;
+calculatedNum = 18;
+
+// 5
+
+// Write a statement that assigns to cost the result of 2 + 2 * 4 + 10,
+// clarified with parentheses, producing 56.
+
+var cost = (2+2)*(4+10)
+cost = 56;
+
+// 6 
+
+// Write a statement that assigns to units the result of 2 + 2 * 4 + 10,
+// clarified with parentheses, producing 20.
+
+var units = 2 + (4 * 2) + 10;
+units = 20;
+
+// 7 
+
+// Write a statement that assigns to pressure the result of 4 / 2 * 4,
+// clarified with parentheses, producing 5.
+
+var pressure = 4 / 2 * 4;
+pressure= error
+
+
+// chapter 8 
+
+// 1
+
+// var num = "2" + "2";
+// What is the value of num? Include quotation marks.
+
+var num = "2" + "2"
+num = 22;
+
+// 2
+
+// message = ("Hello," + "Dolly");
+// What is the value of message? (Include the quotation marks.)
+// Alert the statement
+
+ var message = ("Hello," + "Dolly");
+//  message = HelloDolly;
+ alert(message);
+
+//  3
+
+//  alert("33" + 3);
+// What message displays in the alert box?
+
+333
+
+// 4 
+
+// Write an alert that displays the concatenation of the two parts of
+// "Pakistan Zindabad".
+
+var pakItan = ("Pakistan" + "Zindabad");
+alert(pakItan);
+
+// 5
+
+// Write a statement that assigns to a variable the concatenation of
+// a string with a number
+
+
+var y= "5" + 5;
+
+// 6
+
+// Assign strings to two variables. Then concatenate them and assign
+// the result to a third variable.
+
+var x = "Amna";
+var y = 10;
+var z = x + y;
+alert (z);
+z=Amna10;
+
+// chapter 9
+
+// 1
+
+// Code a prompt with the message "Enter first name". The user's
+// response is assigned to firstName.
+
+
+var firstName = promt("Enter first name");
+alert (firstName);
+
+// 2
+
+// 2. Code a prompt with the message "Country?" and the default
+// answer "China". The user's response is assigned to country.
+
+var yourCountry = prompt("Country?" , "China");
+
+// 3
+
+// 3. Correct this statement
+// var yourName = prompt(Enter Your Name");
+
+
+var yourName = prompt("Enter Your Name");
+
+// 4 
+
+// 4. Code a prompt that specifies a string as the message Include a
+// default input.
+
+var newMessage = prompt("Enter your message" , "Hello World!");
+
+// 5
+
+var message = "Please enter your age:";
+var defaultResponse = "18";
+var userResponse = prompt("message", "defaultResponse");
+
+// chapter 10
+
+// 1 
+
+var city = "Karachi";
+if (city == "Karachi") {
+    alert("The City of Lights");
+
+
+}
+
+// 3 
+
+// Code an if statement that tests if ZipCode is "10010" so, Alert
+// that "Karachi". if not then alert ("Please write correct city")
+
+
+var ZipCode = "10010";
+if (ZipCode == True){
+    alert ("Karachi");
+}
+
+else  {
+    alert("Please write correct city");
+}
+
+// 4
+
+// chapter 11 
+
+// 1
+
+var a = 5;
+if (a != 5){
+
+}
+
+
+
+
+// 2
+
+var a = 2;
+if (a >= 2){
+
+}
+
+// 3 
+
+var x = 10;
+
+if (x != 5) {
+    x = 6
+}
+
+// 4
+
+var number1 = 10;
+var number2 = 20;
+
+if (number1 !== number2) {
+    alert("Congratulations!");
+}
+    
+
+// 5 
+
+var firstName = prompt("Please enter your first name:");
+var anotherName = "John";
+
+if (firstName !== anotherName) {
+    alert("No match");
+}
+
+// chapter 12 
+
+// 1
+
+var a = 5;
+if (a===5){
+    alert(true)
+}
+else{
+    alert(false)
+}
+
+// 2
+
+var perc = +prompt("Enter your percentage");
+if (perc >=80 && < 100){
+    document.write("A+")
+}
+else if(perc >=70 && < 80){
+    document.write("A")
+
+}
+else if (perc >=60 && < 70){
+    document.write("B+")
+}
+else {
+    document.write("C")
+}
+
+
+// chapter 15 
+
+// 1
+
+var arr=[];
+
+// 2
+var arr1=["wow"];
+
+// 3 
+
+var alphabet=["h","i","j","k"];
+alphbet [2];
+
+// 4
+var alphabet=["h","i","j","k", "l","m", "n", "o"]
+var totalLength=7
+
+// chapter 16 
+
+// 1 
+
+var arr=["cow"];
+arr.push("cat");
+
+// 2
+var Alphabet=["h","i","j","k"]
+// Remove the last element from the array Alphabet.
+ Alphabet.pop();
+//   3
+
+var Alphabet=["h","i","j","k"]
+// Add a new element, a number, to the end of an array.
+
+Alphabet.push("2");
+
+// chapter 16 |||
+
+// 1
+
+var sizes = ["S", "M", "XL", "XXL", "XXXL"].
+// Remove the first element of an array.
+sizes.shift();
+
+// 2 
+var sizes = ["S", "M", "XL", "XXL", "XXXL"].
+// Add three number elements to the beginning of an array.
+sizes.unshift("M1","M2","M3");
+
+// 4
+var sizes = ["S", "M", "XL", "XXL", "XXXL"].
+// Insert "L" into the array between "M" and "XL".
+
+sizes.splice(2,0,"L");
+
+// 6
+var pets = ["dog", "cat", "ox", "duck", "frog"].
+// Add 2 elements after "dog" and remove "cat", "ox", and "duck".
+
+pets.splice(1,3,"lion","goat");
+
+// 7
+
+var pets = ["dog", "cat", "ox", "duck", "frog"];
+// Remove "cat" and "ox".
+pets.splice(1,2);
+
+// 8
+
+var pets = ["dog", "cat", "ox", "duck", "frog", "flea"];
+// Reduce it to "duck" and "frog" using slice.
+
+pets.slice(3,5);
+
+
+
+
+
+
+ 
+
+
+
